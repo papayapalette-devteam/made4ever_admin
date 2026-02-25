864ec858d55f0992060ce1e670729d807480baa0
digvijay315 <143862901+digvijay315@users.noreply.github.com>
2026-02-25 14:01:30 +0530

    update'

--- made4ever-backend/controllers/SignIn/otp_sign_in.js
+++ made4ever-backend/controllers/SignIn/otp_sign_in.js
@@ -1,6 +1,7 @@
 const axios =require('axios')
 const MSP = require("../../models/Msp/msp");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey123";
 
